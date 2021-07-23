f7444a0a8863d0a03719d49d2a63ae39e2959aed
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2021-07-23 03:05:20 -0300

    added new method to load user token

--- src/services/load.ts
+++ src/services/load.ts
@@ -5,17 +5,29 @@ interface LoadByCookies {
    * @returns {boolean} the theme loaded by Cookies
    */
   loadTheme: (getThemeByCookie: () => string | undefined) => boolean
+
+  /**
+   * Load the user's token by Cookies
+   * @param {() => string | undefined} getTokenByCookie - a callback that gets the user's token by Cookies and returns it
+   * @returns {string} the user's token loaded by Cookies
+   */
+  loadToken: (getTokenByCookie: () => string | undefined) => string
 }
 
 /**
  * Load stored values by Cookies to init the user's page
  * @method `loadTheme` - Load the theme by Cookies
+ * @method `loadToken` - Load the user's token by Cookies
  */
 export default function load(): LoadByCookies {
   return {
     loadTheme: (getThemeByCookie: () => string | undefined): boolean => {
       const themeLoaded = getThemeByCookie() === 'true' ?? false
       return themeLoaded
+    },
+    loadToken: (getTokenByCookie: () => string | undefined): string => {
+      const tokenLoaded = getTokenByCookie() ?? ''
+      return tokenLoaded
     }
   }
 }
