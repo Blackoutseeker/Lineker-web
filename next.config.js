6989f298243a62c30d00366c2a35d3249797483f
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2022-03-11 16:22:37 -0300

    Added types from NextConfig

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
+/** @type {import('next').NextConfig} */
 module.exports = {
   esModule: true,
   devIndicators: {
-    autoPrerender: true
+    buildActivity: true
   },
   env: {
     API_KEY: process.env.API_KEY,
@@ -11,8 +12,10 @@ module.exports = {
     STORAGE_BUCKET: process.env.STORAGE_BUCKET,
     MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
     APP_ID: process.env.APP_ID,
+
     PRIVATE_KEY: process.env.PRIVATE_KEY,
     CLIENT_EMAIL: process.env.CLIENT_EMAIL,
+
     RECAPTCHA_SITE_KEY: process.env.RECAPTCHA_SITE_KEY,
     RECAPTCHA_SECRET_KEY: process.env.RECAPTCHA_SECRET_KEY
   }
