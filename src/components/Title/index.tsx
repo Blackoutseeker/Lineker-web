7c91f5aef296cb769b2ef353a45692a84e35978d
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2022-03-11 16:31:14 -0300

    Inform the browser not to translate Lineker

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -9,7 +9,7 @@ const Title: FC<TitleProps> = props => {
   const { size } = props
 
   return (
-    <TitleText size={size ?? 24} className={'title'}>
+    <TitleText translate={'no'} size={size ?? 24} className={'title'}>
       Lin<TitleText className={'alternative-title'}>e</TitleText>k
       <TitleText className={'alternative-title'}>er</TitleText>
     </TitleText>
