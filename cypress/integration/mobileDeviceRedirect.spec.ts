eaa4ad5ff8d8fc3b7fd906e562df929781cdd449
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2022-03-12 18:04:12 -0300

    Added history page redirection test

--- cypress/integration/mobileDeviceRedirect.spec.ts
+++ cypress/integration/mobileDeviceRedirect.spec.ts
@@ -1,13 +1,23 @@
 describe('Testing mobile device redirection', () => {
   // pages
-  const loginPageUrl = 'http://localhost:3000/login'
   const homePageUrl = 'http://localhost:3000/'
+  const loginPageUrl = 'http://localhost:3000/login'
+  const historyPageUrl = 'http://localhost:3000/history'
 
-  it("Should redirect to home page if user's using a mobile device", () => {
-    const mobileUserAgentExample =
-      'Mozilla/5.0 (Linux; Android 6.0.1; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Mobile Safari/537.36'
+  const mobileUserAgentExample =
+    'Mozilla/5.0 (Linux; Android 6.0.1; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Mobile Safari/537.36'
 
+  it("Should redirect to home page if user's accessing login page and using a mobile device", () => {
     cy.visit(loginPageUrl, {
+      headers: {
+        'user-agent': mobileUserAgentExample
+      }
+    })
+    cy.url().should('equal', homePageUrl)
+  })
+
+  it("Should redirect to home page if user's accessing history page and using a mobile device", () => {
+    cy.visit(historyPageUrl, {
       headers: {
         'user-agent': mobileUserAgentExample
       }
