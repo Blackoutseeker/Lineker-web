53af65f0718e6d02b7f1dbf961d9bf5841526876
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2022-03-13 12:46:22 -0300

    Changed auth module to authProvider

---
+++ src/services/authProvider.tsx
@@ -0,0 +1,45 @@
+import { createContext, useState, useEffect, useContext } from 'react'
+import { setCookie, destroyCookie } from 'nookies'
+import { User } from 'firebase/auth'
+import { defaultAuth } from '@utils/firebaseClient'
+
+const AuthContext = createContext<{ user: User | null }>({
+  user: null
+})
+
+export const AuthProvider = ({ children }: any) => {
+  const [user, setUser] = useState<User | null>(null)
+
+  const saveTokenByCookie = (token: string) => {
+    setCookie(undefined, 'token', token, {
+      path: '/',
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 60 * 60 * 24 * 30 // 30 days
+    })
+  }
+
+  const deleteTokenFromCookie = () => {
+    destroyCookie(undefined, 'token')
+  }
+
+  useEffect(() => {
+    return defaultAuth.onIdTokenChanged(async user => {
+      if (!user) {
+        setUser(null)
+        deleteTokenFromCookie()
+      } else {
+        await user.getIdToken().then(token => {
+          setUser(user)
+          saveTokenByCookie(token)
+        })
+      }
+    })
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+  )
+}
+
+export const useAuth = () => useContext(AuthContext)
