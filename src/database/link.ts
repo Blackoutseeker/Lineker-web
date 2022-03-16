036c8e555b81315ec3019035273880989965feb6
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2022-03-16 18:57:22 -0300

    Added Link database actions

---
+++ src/database/link.ts
@@ -0,0 +1,90 @@
+import { defaultDatabase } from '@utils/firebaseClient'
+import { ref, get, set, remove, onValue, off } from 'firebase/database'
+import LinkItem from '@models/linkItem'
+
+const createFilterIfNotExists = async (
+  uid: string,
+  filter: string,
+  onSuccess?: () => Promise<void> | void,
+  onError?: () => Promise<void> | void
+): Promise<void> => {
+  const filterDatabaseRef = ref(
+    defaultDatabase,
+    `users/${uid}/filters/${filter}`
+  )
+  await set(filterDatabaseRef, {
+    filter
+  })
+    .then(onSuccess)
+    .catch(onError)
+}
+
+export const addNewLinkToDatabase = async (
+  uid: string,
+  filter: string,
+  linkItem: LinkItem,
+  onSuccess?: () => Promise<void> | void,
+  onError?: () => Promise<void> | void
+): Promise<void> => {
+  await createFilterIfNotExists(uid, filter)
+  const linkDatabaseRef = ref(
+    defaultDatabase,
+    `users/${uid}/links/${filter}/${linkItem.datetime}`
+  )
+  await set(linkDatabaseRef, linkItem).then(onSuccess).catch(onError)
+}
+
+export const getAllLinksFromDatabase = async (
+  uid: string,
+  filter: string
+): Promise<LinkItem[] | null> => {
+  const linkItemsDatabaseRef = ref(
+    defaultDatabase,
+    `users/${uid}/links/${filter}`
+  )
+  return await get(linkItemsDatabaseRef).then(snapshot => {
+    const snapshotIsNotEmpty =
+      snapshot.val() !== null && snapshot.val() !== undefined
+    if (snapshotIsNotEmpty) {
+      const linkItems: LinkItem[] = Object.values(snapshot.val())
+      return linkItems
+    }
+    return null
+  })
+}
+
+export const removeLinkFromDatabase = async (
+  uid: string,
+  filter: string,
+  linkDatetime: string,
+  onSuccess?: () => Promise<void> | void,
+  onError?: () => Promise<void> | void
+): Promise<void> => {
+  const linkItemDatabaseRef = ref(
+    defaultDatabase,
+    `users/${uid}/links/${filter}/${linkDatetime}`
+  )
+  await remove(linkItemDatabaseRef).then(onSuccess).catch(onError)
+}
+
+export const databaseLinksListener = (
+  databaseRef: string,
+  setLinkItems: (linkItems: LinkItem[] | null) => void
+) => {
+  const databaseListenerRef = ref(defaultDatabase, databaseRef)
+
+  return {
+    listen: () =>
+      onValue(databaseListenerRef, snapshot => {
+        const snapshotIsNotEmpty =
+          snapshot.val() !== null && snapshot.val() !== undefined
+        if (snapshotIsNotEmpty) {
+          const linkItems: LinkItem[] = Object.values(snapshot.val())
+          setLinkItems(linkItems)
+        } else {
+          setLinkItems(null)
+        }
+      }),
+    unlisten: () => off(databaseListenerRef)
+  }
+}
